2d6139603e4455c4348303e1725aa9adb2bc1e4e
18XiWenjuan <57065679+18XiWenjuan@users.noreply.github.com>
2022-04-18 11:39:18 +0800

    Add files via upload

---
+++ run_lmf.sh
@@ -0,0 +1,7 @@
+python test.py --emotion happy --audio_hidden 4 --video_hidden 16 --text_hidden 128 --audio_dropout 0.3 --video_dropout 0.1 --text_dropout 0.5 --rank 1 --check_path check/happy.pkl
+
+python test.py --emotion sad --audio_hidden 8 --video_hidden 4 --text_hidden 128 --audio_dropout 0 --video_dropout 0 --text_dropout 0 --rank 4 --check_path check/sad.pkl
+
+python test.py --emotion angry --audio_hidden 8 --video_hidden 4 --text_hidden 64 --audio_dropout 0.3 --video_dropout 0.1 --text_dropout 0.15 --rank 8 --check_path check/angry.pkl
+
+python test.py --emotion neutral --audio_hidden 32 --video_hidden 8 --text_hidden 64 --audio_dropout 0.2 --video_dropout 0.5 --text_dropout 0.2 --rank 16 --check_path check/neutral.pkl
